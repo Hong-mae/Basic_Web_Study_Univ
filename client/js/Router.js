e2287add98ac84af3bf600ec6fa084e25d0e24f3
P-bin <qkrgksqls9@gmail.com>
2016-06-08 06:57:09 +0900

    에러나서 다시 씀

--- client/js/Router.js
+++
@@ -1,8 +0,0 @@
-Router.configure({
-  layoutTemplate: 'layout'
-});
-
-Router.route('/', {name:'home'});
-Router.route('/contact', {name:'contact'});
-Router.route('/press', {name:'press'});
-Router.route('/story', {name:'story'});
