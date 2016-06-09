52b4830feedfe39f78adad867d14d0e97c1631fb
dongryuckkim <ehdfur9364@naver.com>
2016-06-09 10:37:26 +0900

    http 로 이미지 가져오기
    * 잘안됨

--- client/js/search.js
+++ client/js/search.js
@@ -3,6 +3,21 @@
  */
 import '../NavElement/story.html';
 
+import '../../imports/ui/addStory';
+
+Template.story.rendered= function(){
+    Meteor.call("unImage", function(error, results) {
+        Session.set('url', results.data.url);
+    });
+}
+
+
+Template.story.helpers({
+    randomImage: function() {
+        return Session.get('url');
+    }
+});
+
 Template.story.events({
    'submit #search': function(event){
        event.preventDefault();
