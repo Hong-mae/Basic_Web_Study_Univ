c9502c76ff3e5432669bd6c70e7ac122f3d632a6
P-bin <qkrgksqls9@gmail.com>
2016-06-17 06:27:01 +0900

    hashTag Search
    * 해쉬태그 검색

--- client/js/search.js
+++ client/js/search.js
@@ -4,23 +4,35 @@
 import '../template/story.html';
 
 import { Storys } from '../../imports/api/storys';
+import { Tracker } from 'meteor/tracker';
 
 Session.setDefault('img_src');
+Session.setDefault('hash','');
 
-
+Tracker.autorun(function(){
+    Meteor.call("unImage", function(error, results) {
+        Session.set('img_src', results);
+    });
+});
 
 Template.random.helpers({
     randomImage: function() {
         var src =Session.get('img_src');
         return src;
     },
-
 });
 
 
 Template.story.helpers({
     storys(){
-        return Storys.find({});
+        var check = Session.get('hash');
+
+        if(check === '') {
+            return Storys.find({},{sort: {createdAt: -1}});
+        }else{
+            console.log(check);
+            return Storys.find({hash_Tag:{$elemMatch:{tag:check}}}, {sort: {createdAt: -1}}) ;
+        }
     },
 });
 
@@ -32,6 +44,8 @@ Template.story.events({
         var values = target.hashtag.value;
 
         console.log(values);
+
+        Session.set('hash', values);
     },
     'submit #addStory': function(event){
         event.preventDefault();
@@ -40,13 +54,23 @@ Template.story.events({
         var values = target.storyString.value;
         var imageURL = $('#imgload').attr('src');
 
+        var hashtag = $('#hash_Tag').val();
+        var splitArray = hashtag.split(' ');
+        var tags = new Array();
 
+        for(var word in splitArray){
+            word = splitArray[word];
+            if(word.indexOf('#') == 0){
+                tags.push({tag : word});
+            }
+        }
 
         Storys.insert({
             values,
             createdAt: new Date(),
             owner: Meteor.userId(),
             url: imageURL,
+            hash_Tag: tags,
         });
 
         Meteor.call("unImage", function(error, results) {
@@ -55,6 +79,7 @@ Template.story.events({
         });
 
         target.storyString.value = "";
+        target.hash_Tag.value = "";
     },
     'click #refresh':function(event){
         Meteor.call("unImage", function(error, results) {
